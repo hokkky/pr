474c06a340ec624eae5e24bef9b52bda770cddf0
Matvey Pavlov <Matvey.Pavlov2@yandex.ru>
2023-10-21 13:35:05 +0300

    Add gitignore and somrthing more

--- inpoutp.py
+++ inpoutp.py
@@ -4,9 +4,6 @@ def inp():
     return matrix
 
 def outp(m):
-    for i in range(len(m)):
-        for j in range(len(m[i])):
-            print(m[i][j], end=' ')
-        print('\n')
+    print(*m,sep='\n')
 
 outp(inp())
